45a52ce8d2fb786d60b084dc38b0dc8604ac4820
gianvin <ginamangahassanchezong@gmail.com>
2023-09-15 20:44:26 +0800

    did the core requirements for preparation 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,41 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentag? ");
+        string grade = Console.ReadLine();
+
+        if (grade >= "90")
+        {   
+           Console.WriteLine("A");
+        }
+        else if (grade >= "80")
+        {
+           Console.WriteLine("B");
+        }
+        
+        else if (grade >= "70")
+        {
+           Console.WriteLine("C");
+        }
+        else if (grade >= "60")
+        {
+           Console.WriteLine("D");
+        }
+        else 
+        {
+           Console.WriteLine("F");
+       
+        
+        if (grade == "70" || >= "70")
+        { 
+            Console.WriteLine("Congratulations! You passed the course. ");
+        }
+        else
+        {
+            Console.WriteLine("Next time, Study hard. ")
+        }
+
     }
+
+
 }
