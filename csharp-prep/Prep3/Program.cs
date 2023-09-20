c1d18a3de641919b5ac4c524e69ceb789a7eed1e
gianvin <ginamangahassanchezong@gmail.com>
2023-09-20 20:03:49 +0800

    created the Loop

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,29 +1,28 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("What is the magic number? 6");
-        Console.Write("What is your guess? ");
-        string guess = Console.ReadLine();
-        int number = int.Parse(guess);
-        
-        string word = "";
+Console.WriteLine("What is the magic number? 18");
+Console.Write("What is your guess? ");
+string guess = Console.ReadLine();
+int number = int.Parse(guess);
 
-        if (number < 6)
-        {
-           word = "higher";
-        }
-        else if (number > 6)
-        {
-            word = "lower";
-        }
-        else 
-        {
-           word = "You guessed it!";
-        }
-        Console.WriteLine($"{word}");
-    }   
-            
+string word = "";
+
+if (number < 18)
+{
+    word = "higher";
 }
+else if (number > 18)
+{
+    word = "lower";
+}
+else
+{
+    word = "You guessed it!";
+}
+Console.WriteLine($"{word}");
+do
+{
+    Console.Write("What is your guess? ");
+    guess = Console.ReadLine();
+} while (number == 18);
+    Console.Write("You guessed it");
