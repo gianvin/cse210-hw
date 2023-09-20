e8f142a6d0f2fd0e8e4c8fbc99b16ad5840cef44
gianvin <ginamangahassanchezong@gmail.com>
2023-09-20 21:43:22 +0800

    edited after running for correction

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,22 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
-        Console.Write("Enter number: ");
-        string list = Console.ReadLine();
-        int number = int.Parse(list);
-        List<int> numbers;
-        numbers = new List<int>();
-        
-         
-        
-        
+        List<int> numbers = new List<int>();
+        //Do-while list
+        int userNumber = -1;
+        while (userNumber !=0)
         {
-           
+            Console.Write("Enter a number (0 to quit): ");
+            string Response = Console.ReadLine();
+            userNumber = int.Parse(Response);
+            // Add the numbers that are not zero
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
         }
-        
-    }   
-        
-    
-    
+        // Compute the Sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum+= number;
+        }
+        Console.WriteLine($"The sum is: {sum}.");
+        // Compute the Average
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        // Find the maximun
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            { 
+                max = number;
+            }
+        }
+        Console.WriteLine($"The maximum is: {max}");   
+    }       
 }
