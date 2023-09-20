1ddacfe9fa6a0b273013007454fc01738f60e086
gianvin <ginamangahassanchezong@gmail.com>
2023-09-20 21:17:22 +0800

    created the program prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        Console.Write("Enter number: ");
+        string list = Console.ReadLine();
+        int number = int.Parse(list);
+        List<int> numbers;
+        numbers = new List<int>();
+        
+         
+        
+        
+        {
+           
+        }
+        
+    }   
+        
+    
+    
 }
