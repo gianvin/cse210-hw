6f0a00c331eb675a002662439e4fc0204c364744
gianvin <ginamangahassanchezong@gmail.com>
2023-09-20 19:20:31 +0800

    edited prog.3 as guess the number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        Console.WriteLine("What is the magic number? 6");
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+        int number = int.Parse(guess);
+        
+        string word = "";
+
+        if (number <= 6)
+        {
+           word = "higher";
+        }
+        else if (number >= 6)
+        {
+            word = "lower";
+        }
+        else
+        {
+            word = "You guessed it!";
+        }
+    
+    } 
+
+
+
+
 }
