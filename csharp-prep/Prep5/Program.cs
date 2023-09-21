55d148d055dab8e7aa277dc3fd6edbbace4c1315
gianvin <ginamangahassanchezong@gmail.com>
2023-09-21 18:30:45 +0800

    created the assignment for Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,22 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    
+    //Display Function
+    class Square 
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void Main() {
+            Console.Write("\n\n");
+            Console.Write("Welcome to the Program!");
+            Console.ReadLine();
+            Console.Write($"Please enter your name: ");
+        string userName = Console.ReadLine();
+            Console.Write("Enter your number: ");
+           string input = Console.ReadLine();
+          int number = Convert.ToInt32(input);
+          int squared = number * number;
+          Console.Write("\n\n");
+            Console.WriteLine($"{userName}, the square of your number is {squared}");
+        }
     }
-}
+}    
