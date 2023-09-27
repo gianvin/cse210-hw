3298f4916d0b27ea543c839bfae4e7d96792407b
gianvin <ginamangahassanchezong@gmail.com>
2023-09-27 20:05:51 +0800

    created the Job Class

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,26 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    
+     // A code template for the category of things known as Job. The
+    // responsibility of a Job is to hold and display Company information.
+    public class Job
     {
-        Console.WriteLine("Hello Learning02 World!");
-    }
+        // The C# convention is to start member variables with an underscore _
+        public string _jobTitle = "";
+        public string _companyName = "";
+        public string _startYear = "";
+        public string _endYear = "";
+   // A special method, called a constructor that is invoked using the  
+        // new keyword followed by the class name and parentheses.
+        public Job()
+        {  
+        } 
+    // A method that displays the the title of the job 
+       
+        public void ShowJobTitle()
+        {
+            Console.WriteLine($"{_jobTitle}");
+        }
+    } 
 }
