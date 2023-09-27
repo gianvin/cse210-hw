6220409ebf6fc9f6c4ca5e4d54b08470d9365e65
gianvin <ginamangahassanchezong@gmail.com>
2023-09-27 20:56:08 +0800

    Generated the jobtitle etc.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -3,25 +3,45 @@ using System;
 class Program
 {
     static void Main(string[] args)
-     // A code template for the category of things known as Job. The
-    // responsibility of a Job is to hold and display Company information.
-    public class Job
     {
-        // The C# convention is to start member variables with an underscore _
-        public string _jobTitle = "";
-        public string _companyName = "";
-        public string _startYear = "";
-        public string _endYear = "";
-   // A special method, called a constructor that is invoked using the  
-        // new keyword followed by the class name and parentheses.
-        public Job()
-        {  
-        } 
-    // A method that displays the title of the job 
-       
-        public void ShowJobTitle()
-        {
-            Console.WriteLine($"{_jobTitle}");
-        }
-    } 
+
+        Job job1 = new Job();
+        job1._jobTitle = "Teacher";
+        job1._company = "Santulan Elementary School";
+        job1._startYear = 1998;
+        job1._endYear = 2009;
+
+        Job job2 = new Job();
+        job2._jobTitle = "School Head";
+        job2._company = "SDO Mlabon City";
+        job2._startYear = 2009;
+        job2._endYear = 2016;
+
+        Resume myResume = new Resume();
+        myResume._name = "Gina S. Ong";
+
+        myResume.jobs.Add(job1);
+        myResume.jobs.Add(job2);
+// A method that displays the Resume
+        myResume.Display();
+   }
+
+    private class Job
+    {
+        internal string _jobTitle;
+        internal string _company;
+        internal int _endYear;
+        internal int _startYear;
+    }
+}
+
+internal class Resume
+{
+    internal string _name;
+    internal object jobs;
+
+    internal void Display()
+    {
+        throw new NotImplementedException();
+    }
 }
