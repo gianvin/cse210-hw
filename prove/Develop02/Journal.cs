cec7f4cff4ed4d66608efbdc32c7f54bc9628378
gianvin <ginamangahassanchezong@gmail.com>
2023-10-06 21:10:43 +0800

    add the display method

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -3,4 +3,9 @@ public class Journal
     public string _date;
     public string _prompt;
     public string _response;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_date} ({_prompt})~{_response}");
+    }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,23 +1,34 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
+using System.IO;
+string filename = "myFile.txt";
+string[] lines = System.IO.File.ReadAllLines(filename);
 
-class Program
+foreach (string line in lines)
 {
-    static void Main(string[] args)
-    {
-        DateTime theCurrentTime = DateTime.Now;
-string dateText = theCurrentTime.ToShortDateString();
-        Console.WriteLine("Welcome to the Journal Program! ");
-        Console.WriteLine("Please select one of the following choices: ");
-        Console.WriteLine(" 1. Write");
-        Console.WriteLine(" 2. Display");
-        Console.WriteLine(" 3. Load");
-        Console.WriteLine(" 4. Save");
-        Console.WriteLine(" 5. Quit");
-        Console.Write("What would you like to do? ");
-        string choice = Console.ReadLine();
-
-
-        
-
-    }
+    string[] parts = line.Split(",");
+    string _date = parts[0];
+    string _prompt = parts[1];
+    string _response = parts[2];
 }
+DateTime theCurrentTime = DateTime.Now;
+string dateText = theCurrentTime.ToShortDateString();
+
+Console.WriteLine("Welcome to the Journal Program! ");
+Console.WriteLine("Please select one of the following choices: ");
+Console.WriteLine(" 1. Write");
+Console.WriteLine(" 2. Display");
+Console.WriteLine(" 3. Load");
+Console.WriteLine(" 4. Save");
+Console.WriteLine(" 5. Quit");
+Console.Write("What would you like to do? ");
+string choice = Console.ReadLine();
+int prompt = int.Parse(choice);
+
+string prompt = "";
+
+if (choice == 1)
+{
+    prompt = "Who was the most interesting person I interacted with today?";
+}
+else if (choice == 2)
