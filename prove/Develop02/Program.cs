0d630ba18ee796415227bb8922475505bdc76466
gianvin <ginamangahassanchezong@gmail.com>
2023-10-07 20:44:01 +0800

    Created Menu

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -2,17 +2,32 @@ using System;
 using System.Collections;
 using System.Net;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the Journal Program! ");
-        List<string> menu = new List<string>();
-        menu.Add("1. Write"); 
-        menu.Add("2. Display"); 
-        menu.Add("3. Load"); 
-        menu.Add("4. Save"); 
-        menu.Add("5. Quit"); 
+        // Menu
+    public int _choice = 0;
+
+    public Menu()
+    {
+
     }
-    
+    public void DisplayMenu()
+    {
+        Console.WriteLine("Welcome to the Journal Program! ");
+        Console.WriteLine("1. Write"); 
+        Console.WriteLine("2. Display"); 
+        Console.WriteLine("3. Load"); 
+        Console.WriteLine("4. Save"); 
+        Console.WriteLine("5. Quit");
+    }
+
+        
+        List<string> menu = new List<string>();
+         
+    }
+   
 }
