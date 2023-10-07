b3237990d71c107fdfc9fd6d84e8775cc6012196
gianvin <ginamangahassanchezong@gmail.com>
2023-10-07 20:27:29 +0800

    moved two files to recylce bin

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,33 +1,18 @@
 using System;
 using System.Collections;
-
+using System.Net;
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to the Journal Program! ");
-        Prompt Prompt = new Prompt();
-        Journal Journal = new Journal();
-        do{
-
-        Prompt.DisplayPrompt();
-        do{
-            Prompt.UpdateUserChoice();
-        }while((Prompt._response > 5) || (Prompt._choice < 1));
-
-        switch (Prompt._response)
-        case 1:
-            Journal.WriteJournal();
-        break;
-        case 2:
-            Journal.DisplayJournal();
-        break;
-        case 3
-            Journal.SaveJournal();
-        break;
-        case 4
-            Journal.LoadJournal
-        }
+        List<string> menu = new List<string>();
+        menu.Add("1. Write"); 
+        menu.Add("2. Display"); 
+        menu.Add("3. Load"); 
+        menu.Add("4. Save"); 
+        menu.Add("5. Quit"); 
     }
     
 }
