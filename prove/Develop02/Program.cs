b446bce7f378d82eeb84e700115ad19136dc5370
gianvin <ginamangahassanchezong@gmail.com>
2023-10-11 19:12:51 +0800

    make a comment on the errors to run the menu

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -18,61 +18,48 @@ class Program
         "If I had one thing I could do over today, what would it be?"};
         // Write the choices for the user
         while (response == "yes" )
-        Console.WriteLine("Please, select one of the following choices:");
-        Console.WriteLine("1. Write"); 
-        Console.WriteLine("2. Display"); 
-        Console.WriteLine("3. Load"); 
-        Console.WriteLine("4. Save"); 
-        Console.WriteLine("5. Quit");
-        string userInput = Console.ReadLine();
-        int userChoice = int.Parse(userInput);
-        if (userChoice) == 1)
-        {
-            Write writeNew = new Write();
-            writeNew._date = date;
-            writeNew._journal = journal;
-            writeNew._prompts = _prompts;
-            response = "yes";
+            Console.WriteLine("Please, select one of the following choices:");
+            Console.WriteLine("1. Write"); 
+            Console.WriteLine("2. Display"); 
+            Console.WriteLine("3. Load"); 
+            Console.WriteLine("4. Save"); 
+            Console.WriteLine("5. Quit");
+            string userInput = Console.ReadLine();
+            int userChoice = int.Parse(userInput);
+
+            //if (userChoice) == 1)
+               // {
+                  //  Write writeNew = new Write();
+                   // writeNew._date = date;
+                    //writeNew._journal = journal;
+                    //writeNew._prompts = _prompts;
+                    //response = "yes";
+                //}
+        //else if (userChoice == 2)
+        //{
+         //   Show displayNew = new Show();
+         //   displayNew._journal = journal;
+          //  displayNew.Display();
+          //  response = "yes";
+        //}
+        //else if (userChoice == 3)
+       // {
+         //   Load loadNew = new Load();
+           // loadNew._jpurnal = journal;
+           // loadNew.Display();
+           // response = "yes";
         }
-        else if (userChoice == 2)
-        {
-            Show displayNew = new Show();
-            displayNew._journal = journal;
-            displayNew.Display();
-            response = "yes";
-        }
-        else if (userChoice == 3)
-        {
-            Load loadNew = new Load();
-            loadNew._jpurnal = journal;
-            loadNew.Display();
-            response = "yes";
-        }
-        else if (userChoice == 4)
-        {
-            Save saveNew = new Save();
-            saveNew._journal = journal;
-            saveNew.Display();
-            response = "yes";
-        }
-       else if (userChoice == 5)
-       {
-        response = "no";
-       }
-    }  
-   public static void SavetoFile(List<Journal>_entry);  
-   {
-    Console.WriteLine("Saving to file...");
-    string filename = Journal.txt;
-    using (StreamWriter outputFile = new StreamWriter(filename))
-    {
-        foreach (Entry e in Journal)
-        {
-            outputFile.WriteLine($"{e._date}~~{e._prompt}~~{e. _response});
-        }
-    }
-   }
-    
-    } 
-    }
+        //else if (userChoice == 4)
+        //{
+          //  Save saveNew = new Save();
+           // saveNew._journal = journal;
+            //saveNew.Display();
+            //response = "yes";
+        //}
+       //else if (userChoice == 5)
+       //{
+       // response = "no";
+      // }
+    //}  
+  
 }
