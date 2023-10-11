dfd5e0b3880805a4c2c63042c920bfd6e80297b1
gianvin <ginamangahassanchezong@gmail.com>
2023-10-11 20:36:10 +0800

    Added additional variables and methods to Entry.cs

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -20,6 +20,50 @@ public class Show
     public List<string> _journal;
     public void Display()
     {
-
+        for (int i = 0; i < _journal.Count;i++)
+        {
+            string show = _journal [i];
+            Console.WriteLine(show);
+        }
     }
+}
+public class Load
+{
+    public List<string> _journal;
+    private object dateFile;
+
+    public void Display()
+    {
+        Console.WriteLine("What is the file name?");
+        string answer = Console.ReadLine();
+        string[] lines = System.IO.File.ReadAllLines(answer);
+        _journal.Clear();
+        foreach (string line in lines)
+        {
+            string[] parts =line.Split(" - ");
+            string datefile = parts[0];
+            string first = parts[1];
+            string last = parts[2];
+            Console.WriteLine($"{dateFile} - {first}");
+            Console.WriteLine($" - {last}");
+            string entry = ($"{dateFile} - {first} - {last}");
+            _journal.Add(entry);        }
+    }
+  public class Save
+  {
+    public List<string> _journal;
+    public void Display()
+    {
+        Console.WriteLine("What is the file name? ");
+            string answer = Console.ReadLine();
+            using (StreamWriter sw = File.CreateText(answer))
+            {
+                for (int i = 0; i < _journal.Count; i++)
+                {
+                    string entry = _journal[i];
+                    sw.WriteLine(entry);
+                }
+            }
+    }
+  }  
 }
