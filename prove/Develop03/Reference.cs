4c66d16854b732e2bfb427a3b06650141c8ba3e7
gianvin <ginamangahassanchezong@gmail.com>
2023-10-16 20:18:07 +0800

    write the fields in  Reference

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Reference
+{
+    private string book;
+    private string chapter;
+    private string endverse;
+    private string startVerse;
+
+    public Reference(string reference)
+    {
+        
+    }
+
+}
