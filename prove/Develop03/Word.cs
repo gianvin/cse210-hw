08ef3d3348a36b9c99f3ca7c50572552df8e437e
gianvin <ginamangahassanchezong@gmail.com>
2023-10-16 20:24:28 +0800

    wrote the code for the fields in word.cs

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Word
+{
+    private bool isVisible;
+    private string word;
+
+    public Word(string word)
+    {
+    this.word = word;
+    this.isVisible = false;
+    }
+}
