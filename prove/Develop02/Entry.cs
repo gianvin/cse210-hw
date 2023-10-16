2456e0cbe38797924c9d1c66eb512a57e960f4b2
gianvin <ginamangahassanchezong@gmail.com>
2023-10-16 19:53:27 +0800

    created the  journal.cs

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,10 +1,11 @@
 using System.Collections.Generic;
-public class Write
+public class Entry
 {
     public string _date;
-    public List<string> _journal;
     public List<string> _prompts;
+    public List<string> _response;
 
+    public Entry(string _date, string )
     public void Display()
     {
         Random rnd = new Random();
@@ -12,58 +13,6 @@ public class Write
         Console.WriteLine(anyPrompt);
         string answer = Console.ReadLine();
         string entry = ($"Date: {_date} - Prompt: {anyPrompt} - {answer}. ");
-        _journal.Add(entry);
+        _response.Add(entry);
     }
-}
-public class Show
-{
-    public List<string> _journal;
-    public void Display()
-    {
-        for (int i = 0; i < _journal.Count;i++)
-        {
-            string show = _journal [i];
-            Console.WriteLine(show);
-        }
-    }
-}
-public class Load
-{
-    public List<string> _journal;
-    private object dateFile;
-
-    public void Display()
-    {
-        Console.WriteLine("What is the file name?");
-        string answer = Console.ReadLine();
-        string[] lines = System.IO.File.ReadAllLines(answer);
-        _journal.Clear();
-        foreach (string line in lines)
-        {
-            string[] parts =line.Split(" - ");
-            string datefile = parts[0];
-            string first = parts[1];
-            string last = parts[2];
-            Console.WriteLine($"{dateFile} - {first}");
-            Console.WriteLine($" - {last}");
-            string entry = ($"{dateFile} - {first} - {last}");
-            _journal.Add(entry);        }
-    }
-  public class Save
-  {
-    public List<string> _journal;
-    public void Display()
-    {
-        Console.WriteLine("What is the file name? ");
-            string answer = Console.ReadLine();
-            using (StreamWriter sw = File.CreateText(answer))
-            {
-                for (int i = 0; i < _journal.Count; i++)
-                {
-                    string entry = _journal[i];
-                    sw.WriteLine(entry);
-                }
-            }
-    }
-  }  
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,19 +1,21 @@
 using System;
-
 using System.Collections.Generic;
-using System.Net;
-using System.Threading.Tasks.Dataflow;
+using System.IO;
+using System.Linq;
+
 
 class Program
 {
+    private static object _prompts;
 
     static void Main(string[] args)
     {
+
         Console.WriteLine("Welcome to the Journal Program! ");
         string date = DateTime.UtcNow.ToString("yyyy-mm-dd");
         string response = "yes";
         List<string> journal = new List<string>();
-        List<string> _prompts = new List<string> {"Who was the most interesting person I interacted with today?", "What was the best part of my day?", 
+        List<string> prompts = new List<string> {"Who was the most interesting person I interacted with today?", "What was the best part of my day?", 
         "How did I see the hand of the Lord in my life today?", "What was the strongest emotion I felt today?",
         "If I had one thing I could do over today, what would it be?"};
         // Write the choices for the user
@@ -23,10 +25,10 @@ class Program
             Console.WriteLine("2. Display"); 
             Console.WriteLine("3. Load"); 
             Console.WriteLine("4. Save"); 
-            Console.WriteLine("5. Quit");
+            Console.WriteLine("5. Edit");
+            Console.WriteLine("6. Quit");
             Console.Write("What would you like to do? ");
-            string userInput = Console.ReadLine();
-            int userChoice = int.Parse(userInput);
+            int userChoice = int.Parse(Console.ReadLine());
 
             if (userChoice == 1)
                {
@@ -59,8 +61,71 @@ class Program
                }
             else if (userChoice == 5)
               {
-                 response = "no";
+                 Edit editNew = new Edit();
+                 editNew._journal = journal;
+                 editNew.Display();
+                 response = "yes";
+                
               }
+            else if (userChoice == 6) 
+            {
+              
+            } 
+            else if (userChoice < 0 || userChoice > 6)
+            {
+               response = "no";
+            }
     }  
 
+   
+private class Edit
+    {
+        internal List<string> _journal;
+
+        internal void Display()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+internal class Save
+{
+    internal List<string> _journal;
+
+    public Save()
+    {
+    }
+
+    internal void Display()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+internal class Load
+{
+    internal List<string> _journal;
+
+    internal void Display()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+internal class Show
+{
+    internal List<string> _journal;
+
+    internal void Display()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+internal class Write
+{
+    internal string _date;
+    internal List<string> _journal;
+    internal object _prompts;
 }
