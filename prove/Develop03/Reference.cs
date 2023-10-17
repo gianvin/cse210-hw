46afb638d4aaa3629e8c241908d61eff8bc7ff21
gianvin <ginamangahassanchezong@gmail.com>
2023-10-17 19:55:16 +0800

    created the methods code for Reference.cs

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -6,12 +6,47 @@ class Reference
 {
     private string book;
     private string chapter;
-    private string endverse;
+    private string endVerse;
     private string startVerse;
 
     public Reference(string reference)
     {
-        
+       string[] parts = reference.Split(" "); 
+       if (parts.Length == 1)
+       {
+        book = parts[0];
+       }
+       else if (parts.Length == 2)
+       {
+        string[] chapterVerse = parts[1].Split(":");
+        book = parts[0];
+        chapter = chapterVerse[0];
+        string[] verseRange = chapterVerse[1].Split("-");
+        startVerse = verseRange[0];
+        endVerse = verseRange.Length > 1 ? verseRange[1] : startVerse;
+       }
+       else
+       {
+        return;
+       }
+    }
+    public override string ToString()
+    {
+        if (!string.IsNullOrEmpty(book))
+        {
+            if (!string.IsNullOrEmpty(chapter))
+            {
+                return $"{book} {chapter}:{startVerse}";
+            }
+            else
+            {
+                return $"{book} {chapter}:{startVerse}-{endVerse}";
+            }
+        }
+        else
+        {
+           return book; 
+        }
     }
 
 }
