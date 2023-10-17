671417fb9cc21a777929358a65c5852f294e728a
gianvin <ginamangahassanchezong@gmail.com>
2023-10-17 20:03:23 +0800

    created the methods for word.cs

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -4,12 +4,28 @@ using System.Text;
 
 class Word
 {
+    // code for fields
     private bool isVisible;
     private string word;
 
+   //code for methods
     public Word(string word)
     {
     this.word = word;
     this.isVisible = false;
     }
+    public bool GetIsVisible()
+    {
+        return isVisible;
+    }
+
+    public void SetIsVisible(bool isVisible)
+    {
+        this.isVisible = isVisible;
+    }
+    public override string ToString()
+    {
+        return isVisible ? word : "___";
+    }
+
 }
