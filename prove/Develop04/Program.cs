e6befba5caca92efd0268402521eaaefd03cd7ff
gianvin <ginamangahassanchezong@gmail.com>
2023-11-01 15:51:28 +0800

    added s on menu option

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -6,7 +6,7 @@ class Program
     static void Main(string[] args)
     {
         // Write the menu option
-        Console.WriteLine("Menu Option:");
+        Console.WriteLine("Menu Options:");
         Console.WriteLine("1. Start breathing activity");
         Console.WriteLine("2. Start reflecting activity");
         Console.WriteLine("3. Start Listing Actvity");
