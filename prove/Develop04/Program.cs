48286a3256b889169e76f95d61d95aa26df3aa9f
gianvin <ginamangahassanchezong@gmail.com>
2023-11-01 14:57:25 +0800

    Created the menu in Program.cs

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,15 @@
 using System;
+using System.ComponentModel.Design;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Menu Option:");
+        Console.WriteLine("1. Start breathing activity");
+        Console.WriteLine("2. Start reflecting activity");
+        Console.WriteLine("3. Start Listing Actvity");
+        Console.WriteLine("4. Quit");
+        Console.WriteLine("Select a choice from the menu: ");
     }
 }
