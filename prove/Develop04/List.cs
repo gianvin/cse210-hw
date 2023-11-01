c40f638577346d0ece4c576b36c47fae3be75e33
gianvin <ginamangahassanchezong@gmail.com>
2023-11-01 14:46:35 +0800

    created prompts for List Activity

--- prove/Develop04/List.cs
+++ prove/Develop04/List.cs
@@ -2,6 +2,33 @@ using System;
 
 class ListActivity : Activity
 {
+    //fields
     private List<string> PROMPTS;
-    public ListActivity() : base()
+
+    //Methods
+    public ListActivity() : base("Listing", "This activity will help you rflect on the good things in your life by having you list as many things as you can in a certain area.")
+    {
+            PROMPTS = new List<string>
+        {
+           "Who are the people you appreciate?",
+           "What are personal strenghts of yours?",
+           "Who are people that you have helped this week?",
+           "when have you felt the Holy Ghost this month?",
+           "Who are some of your personal heroes?" 
+        }; 
+
+    }
+    private string GetRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(PROMPTS.Count);
+        return PROMPTS[index];
+    }
+    public void Run()
+    {
+        DisplayStartingMessage();
+        Console.WriteLine("List as many responses you can to the following prompt: " + GetRandomPrompt());
+        DisplayEndingMessage();
+
+    }
 }
