c8b7aba67718331a00febdd128a96c79f79d0f8c
gianvin <ginamangahassanchezong@gmail.com>
2023-11-01 15:48:12 +0800

    Write the if statement in Program.cs

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,11 +5,41 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Write the menu option
         Console.WriteLine("Menu Option:");
         Console.WriteLine("1. Start breathing activity");
         Console.WriteLine("2. Start reflecting activity");
         Console.WriteLine("3. Start Listing Actvity");
         Console.WriteLine("4. Quit");
-        Console.WriteLine("Select a choice from the menu: ");
+        Console.Write("Select a choice from the menu: ");
+        int userChoice = int.Parse(Console.ReadLine());
+        // Write the if statement
+        if (userChoice == 1)
+            {
+                Console.WriteLine("Welcome to the Breathing Actvity. ");
+                BreathingActivity breathingActivity = new BreathingActivity();
+                breathingActivity.Run();
+            }
+        else if (userChoice == 2)
+            {
+                Console.WriteLine("Welcome to the Reflecting Activity. ");
+                ReflectingActivity reflectingActivity = new ReflectingActivity();
+                reflectingActivity.Run();
+            }
+        else if (userChoice == 3)
+            {
+                Console.WriteLine("Welcome to the Listing Activity. ");
+                ListActivity listActivity = new ListActivity();
+                listActivity.Run();
+            }
+        else if (userChoice == 4)
+        {
+            
+        }
+        else
+        {
+            Console.ReadLine();
+        }
+
     }
 }
