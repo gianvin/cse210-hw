8f9df48a7acaf10badac87e95f3edc330b37cfed
gianvin <ginamangahassanchezong@gmail.com>
2023-11-03 16:37:49 +0800

    edited the list activity

--- prove/Develop04/List.cs
+++ prove/Develop04/List.cs
@@ -4,18 +4,23 @@ class ListActivity : Activity
 {
     //fields
     private List<string> PROMPTS;
+    protected List<string> userResponse;
 
     //Methods
     public ListActivity() : base("Listing", "This activity will help you rflect on the good things in your life by having you list as many things as you can in a certain area.")
     {
             PROMPTS = new List<string>
         {
-           "Who are the people you appreciate?",
-           "What are personal strenghts of yours?",
-           "Who are people that you have helped this week?",
-           "When have you felt the Holy Ghost this month?",
-           "Who are some of your personal heroes?" 
+           "---Who are the people you appreciate?---",
+           "---What are personal strenghts of yours?---",
+           "---Who are people that you have helped this week?---",
+           "---When have you felt the Holy Ghost this month?---",
+           "---Who are some of your personal heroes?---" 
         }; 
+
+        userResponse = new List<string>();
+
+        
 
     }
     private string GetRandomPrompt()
@@ -28,7 +33,16 @@ class ListActivity : Activity
     {
         DisplayStartingMessage();
         Console.WriteLine("List as many responses you can to the following prompt: " + GetRandomPrompt());
-        Console.Write("");
+        Console.Write("You may begin in:");
+        Console.WriteLine();
+
+        
+        foreach (var prompt in PROMPTS)
+        {
+            string response = Console.ReadLine();
+            userResponse.Add(response);
+        }
+        Console.WriteLine($"You listed {userResponse.Count} items!");
         DisplayEndingMessage();
 
     }
