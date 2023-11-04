2afa4cb63a78ac8fa0de7120b36262bc37370ed6
gianvin <ginamangahassanchezong@gmail.com>
2023-11-04 15:38:04 +0800

    wrote the code for shape.cs

--- prepare/Learning05/Shape.cs
+++ prepare/Learning05/Shape.cs
@@ -1,1 +1,24 @@
 using System;
+
+public class Shape
+{
+    protected string _color;
+
+    public Shape()
+    {
+
+    }
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+    public float GetArea()
+    {
+        return  -1;
+    }
+}
