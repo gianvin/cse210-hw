b25cf17944baeac2c4126f73476a7658a51cb59c
gianvin <ginamangahassanchezong@gmail.com>
2023-11-14 20:00:28 +0800

    edited the small letter c of timesToComplete

--- prove/Develop05/ChecklistGoal.cs
+++ prove/Develop05/ChecklistGoal.cs
@@ -5,17 +5,27 @@ public abstract class ChecklistGoal : CompletableGoal
     //fields
     private int bonusAmount;
     private int timesCompleted;
-    private int timesTocomplete;
+    private int timesToComplete;
 
     //methods
     public ChecklistGoal(string name, string description, int points, int timesToComplete, int bonusAmount) : base(name, description, points)
     {
-        this.timesTocomplete = timesToComplete;
+        this.timesToComplete = timesToComplete;
         this.bonusAmount = bonusAmount;
     }
     public ChecklistGoal(string savedString) : base(savedString)
     {
 
+    }
+    public override int RecordEvent()
+    {
+      timesCompleted++;
+      if (timesCompleted == timesToComplete)
+      {
+        isComplete = true;
+        points += bonusAmount;
+      }
+      return bonusAmount;
     }
 
 
