cedd6a3e998cc4b769f71f9937e6c1d4b9e03bd1
gianvin <ginamangahassanchezong@gmail.com>
2023-11-14 19:13:22 +0800

    wrote the code for methods of abstract class Goal

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -1,13 +1,15 @@
 using System;
 using System.Diagnostics;
 
-abstract class Goal
-{
+public abstract class Goal
+{ //fields
    protected string description;
 
    protected int points;
 
    public string Name {get; set;}
+
+   //methods
 
    public Goal(string name, string description, int points)
    {
@@ -16,6 +18,24 @@ abstract class Goal
     this.points = points;
    }
 
+   public Goal(string savedString)
+   {
 
-   
+   }
+   public abstract int RecordEvent();
+
+   public virtual string ToSaveString()
+   {
+      return $"{Name},{description}, {points}";
+   }
+
+    public override string ToString()
+    {
+        return $"{Name}: {description}  ({points} points)";
+    }
+
+
+
+
+
 }
