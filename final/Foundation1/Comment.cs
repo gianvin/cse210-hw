2317375810843b38ef466dc9c82efc45b6daa8f4
gianvin <ginamangahassanchezong@gmail.com>
2023-11-29 14:14:58 +0800

    moved  final project to the folder Final

--- final/Foundation1/Comment.cs
+++ final/Foundation1/Comment.cs
@@ -6,7 +6,7 @@ class Comment
     public string Author {get; set;}
     public string Text {get; set;}
 
-    public Comment(string authos, string text)
+    public Comment(string author, string text)
     {
         Author = author;
         Text = text;
--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        Video myVideo = new Video(",");
+
+    myVideo.AddComment(",");
+
+    myVideo.SetLengthInseconds(300);
+
+    Console.WriteLine($"Video Title: {myVideo.Title}");
+    Console.WriteLine($"Video Author: {myVideo.Author}");
+    Console.WriteLine($"Video Length: {myVideo.LengthInSeconds} seconds");
+    Console.WriteLine($"Number of Comments: {myVideo.GetNumberofComments()}");
+
+    Console.WriteLine("Comments:");
+    foreach (Comment comment in myVideo.Comments)
+    {
+        Console.WriteLine($"{comment.Author}: {comment.Text}");
+    }
     }
 }
