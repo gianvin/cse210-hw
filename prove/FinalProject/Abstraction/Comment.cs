7f813a5958192660d88fe7a96b8d7b444d68a634
gianvin <ginamangahassanchezong@gmail.com>
2023-11-29 13:38:42 +0800

    wrote the code for comment

--- prove/FinalProject/Abstraction/Comment.cs
+++ prove/FinalProject/Abstraction/Comment.cs
@@ -3,4 +3,12 @@ using System;
 class Comment
 {
     
+    public string Author {get; set;}
+    public string Text {get; set;}
+
+    public Comment(string authos, string text)
+    {
+        Author = author;
+        Text = text;
+    }
 }
--- prove/FinalProject/Abstraction/Video.cs
+++ prove/FinalProject/Abstraction/Video.cs
@@ -2,5 +2,4 @@ using System;
 
 class Video
 {
-    
 }
