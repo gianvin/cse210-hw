5e0e75f0292ec83ec193c49fa38853023e175b26
gianvin <ginamangahassanchezong@gmail.com>
2023-11-29 13:50:33 +0800

    added method on comment

--- prove/FinalProject/Abstraction/Video.cs
+++ prove/FinalProject/Abstraction/Video.cs
@@ -2,4 +2,28 @@ using System;
 
 class Video
 {
+    public string Title { get; set}
+    public string Author { get; set;}
+
+    public List<Comment> comments = new List<Comment>();
+    public int LengthInSeconds {get; set;}
+
+    public Video(string title, string author)
+    {
+        Title - title;
+        Author = author;
+    }
+
+    public void AddComment(string author, text);
+    {
+        Comment comment = new Comment(author, text);
+        comment.Add(comment);
+    }
+    public int GetNumberOfComments()
+    {
+        return comments.Count;
+    }
+
+
+
 }
