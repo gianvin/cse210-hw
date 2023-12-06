5c51e58eed3885240e71dd64ae92218f6dcea7db
gianvin <ginamangahassanchezong@gmail.com>
2023-12-06 19:49:40 +0800

    created code for Stationary Bicycles. cs

--- final/Foundation4/StationaryBicycle.cs
+++ final/Foundation4/StationaryBicycle.cs
@@ -3,9 +3,22 @@ using System.Collections.Generic;
 
 class StationaryBicycles : Activity
 {
+    //Fields
     private double speed;
     public double Speed
     {
-        
+        get { return speed; }
+        private set { speed = value; }
+    }
+
+    public StationaryBicycles(DateTime date, double speed)
+        : base(date, "Stationary Bicycles")
+    {
+        Speed = speed;
+    }
+
+    public override void DisplaySummary()
+    {
+        Console.WriteLine($"{Date: dd MMMM yyyy} - {Area} - Speed {Speed} km/h.");
     }
 }
